fb39780f3312e3d015d9cb83221f75d42cc35619
Micah Walter <micah@micahwalter.com>
2013-05-06 16:22:57 -0400

    rewriting this a bit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: newrelic-admin run-program gunicorn shannon:app -w 3
+web: gunicorn shannon:application
--- shannon.py
+++ shannon.py
@@ -1,6 +1,8 @@
 import Image
 import math
 import cgi
+from cgi import escape
+import re
 import json
 import cStringIO
 import urllib
@@ -19,85 +21,64 @@ def shannon_entropy(img):
 
 	return -sum([p * math.log(p, 2) for p in samples_probability if p != 0])
 
+def index(environ, start_response):
+	start_response('200 OK', [('Content-Type', 'text/html')])
+	return ['''Shannon Entropy''']
 
-def app(environ, start_response):
-	data = ''	
+def json_response(environ, start_response):
+
 	status = '200 OK'
 	rsp = {}
-	
-	# determine the size of the request_body, and grab any params
-	
-	try:
-		request_body_size = int(environ.get('CONTENT_LENGTH', 0))
-		params = cgi.parse_qs(environ.get('QUERY_STRING', ''))
-		path = params.get('path', None)
-	except Exception, e:
-		logging.error(e)
-		request_body_size = 0
-	
-	# look for data as either a POST or path param
-	
-	if request_body_size!=0:
-		form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
-		fileitem = form['imageupload']	
-		data = fileitem.file
-	elif path:
+
+	params = cgi.parse_qs(environ.get('QUERY_STRING', ''))
+
+	path = params.get('path', None)
+
+	if not path:
+		rsp = {'stat': 'error', 'error': 'missing image'}
+
+	else:
 		path = path[0]
-		path = urllib.unquote(path)
-		data = cStringIO.StringIO(urllib.urlopen(path).read())
-		
-		
-		
-	# open the image and calculate its shannon entropy
-		
-	try:
-		im = Image.open(data)
-		rsp['shannon-entropy'] = shannon_entropy(im)
-		rsp['stat'] = 'ok'
-	except Exception, e:
-		logging.error(e)
-		rsp = {'stat': 'error', 'error': "failed to process image: %s" % e}
-			
+
+		try:
+			rsp['stat'] = 'ok'
+			rsp['shannon'] = shannon_entropy(path)	
+		except Exception, e:
+			logging.error(e)
+			rsp = {'stat': 'error', 'error': "failed to process image: %s" % e}
+
 	if rsp['stat'] != 'ok':
 		status = "500 SERVER ERROR"
-	else:		
-		rsp = json.dumps(rsp)
+
+	rsp = json.dumps(rsp)
 		
-		logging.debug(status)
+	logging.debug("%s : %s" % (path, status))
+
+	start_response(status, [
+            ("Content-Type", "text/javascript"),
+            ("Content-Length", str(len(rsp)))
+            ])
+
+	return iter([rsp])
+
+
 	
-		# return the result as json 
-		
-		start_response("200 OK", [
-					("Content-Type", "text/javascript"),
-					("Content-Length", str(len(rsp)))
-					])
-		
-		return iter([rsp])
-					
-	# otherwise we just return the form
+def not_found(environ, start_response):
+	start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
+	return ['Not Found']
 	
-	start_response("200 OK", [
-					("Content-Type", "text/html"),
-					("Content-Length", str(len(html)))
-					])
+
+urls = [
+	(r'^$', index),
+	(r'json/?$', json_response)
+]
+
+def application(environ, start_response):
+	path = environ.get('PATH_INFO', '').lstrip('/')
+	for regex, callback in urls:
+		match = re.search(regex, path)
+		if match is not None:
+			environ['myapp.url_args'] = match.groups()
+			return callback(environ, start_response)
+	return not_found(environ, start_response)
 	
-	return iter([html])
-
-
-
-# basic form for uploading an image
-
-html = """
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Shannon Entropy Server</title>
-	</head>
-	<body>
-		<form id="mainform" enctype="multipart/form-data" action="/" method="post" name="mainform">
-			<label for="imageupload">Image file:</label> <input name="imageupload" id="imageupload" type="file"><br>
-			<input name="submit" id="submit" type="submit" value="submit">
-		</form>
-	</body>
-</html>
-"""
